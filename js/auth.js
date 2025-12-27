8bd15dd7789462f4591ab951ef4c09ef35621cce
malabonventures-business <malabonventures@gmail.com>
2025-12-28 00:09:56 +0800

    Implement user login functionality in auth.js

---
+++ js/auth.js
@@ -0,0 +1,14 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "pos.html";
+    })
+    .catch(err => alert(err.message));
+};
